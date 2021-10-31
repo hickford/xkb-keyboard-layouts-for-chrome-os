42bfbfc3e8e4e6c67985468d50e2ab338af3ce4c
matthickford <mirth.hickford@gmail.com>
2021-10-31 16:53:21 +0000

    read yaml as output by `xkbcli list`
    assumes https://github.com/xkbcommon/libxkbcommon/pull/266
    
    ```
    xkbcli list --skip-default-paths --ruleset=base ~/xkeyboard-config > xkb.yaml
    python3 generate.py xkb.yaml
    ```

--- generate.py
+++ generate.py
@@ -11,9 +11,9 @@ manifest['input_components'] = list()
 
 from typing import List, NamedTuple, Optional
 class ConfigItemDetails(NamedTuple):
-    """Details from a single configItem element"""
+    """Details from a single configItem xml element"""
     name: str
-    brief: Optional[str]
+    shortDescription: Optional[str]
     description: Optional[str]
     """Countries"""
     iso3166: Optional[List[str]]
@@ -37,8 +37,8 @@ class LayoutDetails(NamedTuple):
 
 def merge(parent : ConfigItemDetails, variant : Optional[ConfigItemDetails]) -> LayoutDetails:
     if not variant:
-        return LayoutDetails(layout = parent.name, variant=None, brief = parent.brief, description = parent.description, iso3166 = parent.iso3166, iso639 = parent.iso639)
-    return LayoutDetails(layout=parent.name, variant=variant.name, brief = variant.brief, description=variant.description, iso3166=variant.iso3166 if variant.iso3166 != None else parent.iso3166, iso639 = variant.iso639 if variant.iso639 != None else parent.iso639)
+        return LayoutDetails(layout = parent.name, variant=None, brief = parent.shortDescription, description = parent.description, iso3166 = parent.iso3166, iso639 = parent.iso639)
+    return LayoutDetails(layout=parent.name, variant=variant.name, brief = variant.shortDescription if variant.shortDescription != None else parent.shortDescription, description=variant.description, iso3166=variant.iso3166 if variant.iso3166 != None else parent.iso3166, iso639 = variant.iso639 if variant.iso639 != None else parent.iso639)
 
 import langcodes
 
@@ -62,7 +62,7 @@ def get_config_details(configItem) -> ConfigItemDetails:
     description = configItem.find('description').text if configItem.find('description') is not None else None
     iso3166 = [x.text for x in configItem.findall('countryList/iso3166Id')] if configItem.find('countryList') is not None else None
     iso639 = [x.text for x in configItem.findall('languageList/iso639Id')] if configItem.find('languageList') is not None else None
-    return ConfigItemDetails(name=name, brief=shortDescription, description=description, iso3166=iso3166, iso639=iso639)
+    return ConfigItemDetails(name=name, shortDescription=shortDescription, description=description, iso3166=iso3166, iso639=iso639)
 
 def layouts_from_xml_path(path: str) -> List[LayoutDetails]:
     evdev = etree.parse(path)
@@ -72,8 +72,13 @@ def layouts_from_xml_path(path: str) -> List[LayoutDetails]:
         for variant in layout.findall('variantList/variant/configItem'):
             yield merge(parent, get_config_details(variant))
 
-evdev_path = sys.argv[1] if len(sys.argv) > 1 else "/usr/share/X11/xkb/rules/evdev.xml"
-for layout in layouts_from_xml_path(evdev_path):
+def layouts_from_yaml_path(path: str) -> List[LayoutDetails]:
+    import ruamel.yaml
+    return [LayoutDetails(**layout) for layout in ruamel.yaml.safe_load(open(path))['layouts']]
+
+path = sys.argv[1] if len(sys.argv) > 1 else "/usr/share/X11/xkb/rules/evdev.xml"
+layouts = layouts_from_yaml_path(path) if path.endswith(".yaml") else layouts_from_xml_path(path)
+for layout in layouts:
     append_input_component(layout)
 
 with open('manifest.json', 'w') as f:
